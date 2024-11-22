61bebec9491eb105564f6447d6764b5bac1556d4
Yash sarda <yashsarda2002@gmail.com>
2024-11-22 16:24:31 +0530

    added homepage

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -16,8 +16,17 @@ const Home = () => {
             Tell us what you like (and what not)
           </h3>
           <p className="step-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
-            delectus at quis nisi qui sequi debitis dolorum.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, sunt.
+            Eaque eveniet maiores iste. Consectetur, aliquam quo! Ad atque unde
+            reprehenderit earum delectus repellendus ut consequuntur vero ea
+            fuga aspernatur eum facilis at non, quas magnam quia neque inventore
+            temporibus sequi ex deleniti. Commodi ratione at ea, maxime
+            recusandae accusantium odio, quas voluptates, magnam ipsam sapiente
+            dignissimos enim provident dolore deleniti sequi est vero impedit a
+            eligendi rem praesentium nesciunt! Iure ipsam adipisci expedita
+            magni optio veniam, rerum voluptate aliquam quo iusto delectus,
+            eaque inventore, maxime et. Maiores commodi porro iusto quibusdam?
+            Numquam aliquam vero laboriosam minima at maiores exercitationem..
           </p>
         </div>
         <div className="step-img-box">
@@ -33,8 +42,17 @@ const Home = () => {
           <p className="step-number">02</p>
           <h3 className="heading-tertiary">Approve your weekly meal plan</h3>
           <p className="step-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
-            delectus at quis nisi qui sequi debitis dolorum.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, sunt.
+            Eaque eveniet maiores iste. Consectetur, aliquam quo! Ad atque unde
+            reprehenderit earum delectus repellendus ut consequuntur vero ea
+            fuga aspernatur eum facilis at non, quas magnam quia neque inventore
+            temporibus sequi ex deleniti. Commodi ratione at ea, maxime
+            recusandae accusantium odio, quas voluptates, magnam ipsam sapiente
+            dignissimos enim provident dolore deleniti sequi est vero impedit a
+            eligendi rem praesentium nesciunt! Iure ipsam adipisci expedita
+            magni optio veniam, rerum voluptate aliquam quo iusto delectus,
+            eaque inventore, maxime et. Maiores commodi porro iusto quibusdam?
+            Numquam aliquam vero laboriosam minima at maiores exercitationem..
           </p>
         </div>
         <div className="step-img-box">
@@ -50,8 +68,17 @@ const Home = () => {
           <p className="step-number">03</p>
           <h3 className="heading-tertiary">Receive meals at convenient time</h3>
           <p className="step-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
-            delectus at quis nisi qui sequi debitis dolorum.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, sunt.
+            Eaque eveniet maiores iste. Consectetur, aliquam quo! Ad atque unde
+            reprehenderit earum delectus repellendus ut consequuntur vero ea
+            fuga aspernatur eum facilis at non, quas magnam quia neque inventore
+            temporibus sequi ex deleniti. Commodi ratione at ea, maxime
+            recusandae accusantium odio, quas voluptates, magnam ipsam sapiente
+            dignissimos enim provident dolore deleniti sequi est vero impedit a
+            eligendi rem praesentium nesciunt! Iure ipsam adipisci expedita
+            magni optio veniam, rerum voluptate aliquam quo iusto delectus,
+            eaque inventore, maxime et. Maiores commodi porro iusto quibusdam?
+            Numquam aliquam vero laboriosam minima at maiores exercitationem..
           </p>
         </div>
         <div className="step-img-box">
